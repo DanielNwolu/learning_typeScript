5f974cc3e528a945ce4af73a5cab5e2546ee0a02
DanielNWolu <nwoludaniel.c@gmail.com>
2025-02-23 07:07:11 +0100

    corrected the error in the code

--- index.ts
+++ index.ts
@@ -1,40 +1,65 @@
-// practicing type scripts
-// let dave:string;
-
-// dave="david"
-
-// interface IProps {
-
-// }
-
-
-// type Props = {
-//     name:string;
-// }
-
-// const daniel: Props ={name:"daniel"}
-
-
-// counter function
-
-type CounterProps ={
-    value:number;
-    increament: ()=> void;
-    decreament:()=> void;
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
 }
 
-const counter:CounterProps={
-    value:20,
-    increament:function (){
-        this.value++
-    },
-    decreament:function (){
-        this.value--
-    },
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
 }
 
-counter.increament()
-console.log(counter.value)
 
-counter.decreament()
-console.log(counter.value)
+export type Person = User | Admin;
+
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+];
+
+
+export function logPerson(person: Person) {
+    console.log(
+        ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
+    );
+}
+
+
+export function filterPersons<T extends 'user' | 'admin'>(
+    persons: Person[],
+    personType: T,
+    criteria: Partial<Omit<Extract<Person, { type: T }>, 'type'>>
+): Extract<Person, { type: T }>[] {
+    return persons
+        .filter((person): person is Extract<Person, { type: T }> => person.type === personType)
+        .filter((person) => {
+            const criteriaKeys = Object.keys(criteria) as (keyof typeof criteria)[];
+            return criteriaKeys.every((fieldName) => {
+                return person[fieldName] === criteria[fieldName];
+            });
+        });
+}
+
+
+export const usersOfAge23 = filterPersons(persons, 'user', { age: 23 });
+export const adminsOfAge23 = filterPersons(persons, 'admin', { age: 23 });
+
+
+console.log('Users of age 23:');
+usersOfAge23.forEach(logPerson);
+
+
+console.log();
+
+
+console.log('Admins of age 23:');
+adminsOfAge23.forEach(logPerson);
